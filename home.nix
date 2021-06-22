82d13f6ae2f076327082e77ab656b74b4abd6865
Jamie Macdonald <jamie.alban@gmail.com>
2021-06-22 13:08:19 -0700

    initial commit

---
+++ home.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "jamie";
+  home.homeDirectory = "/home/jamie";
+
+  programs.git = {
+    enable = true;
+    userName = "Jamie Macdonald";
+    userEmail = "jamie.alban@gmail.com";
+  };
+
+  programs.neovim = {
+    enable = true;
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.11";
+}
