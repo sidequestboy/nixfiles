b81c89ea7f3ef83caf4a177833ecf2593be0d214
Jamie Macdonald <jamie.alban@gmail.com>
2022-03-25 17:56:54 -0700

    update

--- home.nix
+++ home.nix
@@ -52,12 +52,51 @@
         '';
       }
       coc-nvim
+      {
+        plugin = goyo;
+        config = ''
+          function! s:goyo_enter()
+            if executable('tmux') && strlen($TMUX)
+              silent !tmux set status off
+              silent !tmux list-panes -F '\#F' | grep -q Z || tmux resize-pane -Z
+            endif
+            set noshowmode
+            set noshowcmd
+            set scrolloff=999
+            " ...
+          endfunction
+
+          function! s:goyo_leave()
+            if executable('tmux') && strlen($TMUX)
+              silent !tmux set status on
+              silent !tmux list-panes -F '\#F' | grep -q Z && tmux resize-pane -Z
+            endif
+            set showmode
+            set showcmd
+            set scrolloff=5
+            " ...
+          endfunction
+
+          autocmd! User GoyoEnter nested call <SID>goyo_enter()
+          autocmd! User GoyoLeave nested call <SID>goyo_leave()
+
+        '';
+      }
     ];
     extraConfig = ''
       let mapleader = ","
+      nnoremap <leader>g :Goyo<cr>
+
       augroup javascript
         autocmd!
+        autocmd BufNewFile,BufRead *.js set ft=javascript
         autocmd FileType javascript setlocal shiftwidth=2 softtabstop=2 expandtab
+      augroup END
+
+      augroup markdown
+        autocmd!
+        autocmd BufNewFile,BufRead *.md set ft=markdown
+        autocmd FileType markdown setlocal shiftwidth=2 softtabstop=2 expandtab
       augroup END
 
       "last position
